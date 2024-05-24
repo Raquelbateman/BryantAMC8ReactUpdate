4bc984c4c306f8ca9019d426cb558db4de15257e
Raquelbateman <aliciaraquel1017@gmail.com>
2024-05-23 21:33:07 -0700

    update

--- reactupdate/src/components/Cart.tsx
+++ reactupdate/src/components/Cart.tsx
@@ -4,8 +4,8 @@ const Cart = () => {
   const [cart, setCart] = useState({
     discount: 0.1,
     items: [
-      { id: 1, title: 'Blue Cheese Olives', quantity: 1 },
-      { id: 2, title: 'Orange Juice', quantity: 1 },
+      { id: 1, title: 'Product: Blue Cheese Olives', quantity: 1 },
+      { id: 2, title: 'Product: Orange Juice', quantity: 1 },
     ],
   });
 
@@ -20,15 +20,11 @@ const Cart = () => {
 
   return (
     <>
-      <h2>Cart</h2>
-      <ul>
-        {cart.items.map((item) => (
-          <li key={item.id}>
-            {item.title} - Quantity: {item.quantity}
-          </li>
-        ))}
-      </ul>
-      <button onClick={handleClick}>Update Cart</button>
+      <h2>Shopping Cart</h2>
+      <span>{cart.items[0].title}:</span>
+      <span>{cart.items[0].quantity}</span>
+      
+      <button onClick={handleClick}>Add To Cart</button>
     </>
   );
 };
